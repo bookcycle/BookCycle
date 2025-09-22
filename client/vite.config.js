0e4e45e3b6309b73f8886e36231ce111112cc2a8
jnkarim <julkernkarim@gmail.com>
2025-09-22 23:52:12 +0600

    update app file

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(({ mode }) => ({
-  base: '/', 
+export default defineConfig({
+  // 👇 repo name since you're serving at /BookCycle/
+  base: "/BookCycle/",
   plugins: [react(), tailwindcss()],
-}));
+});
