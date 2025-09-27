1443b7abe27c63e2c52a5dab6bfc31ac6c586159
jnkarim <julkernkarim@gmail.com>
2025-09-27 11:26:35 +0600

    change base in vite.config.js file

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  // 👇 repo name since you're serving at /BookCycle/
-  base: "/BookCycle/",
+  base: "/",
   plugins: [react(), tailwindcss()],
 });
